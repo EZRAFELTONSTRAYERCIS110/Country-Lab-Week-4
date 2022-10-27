c8421bb4c470deffbb0da3d12d9631d9d4a89052
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-10-26 17:42:16 -0700

    Country Lab Week 4

--- Country_Lab_Week_4.py
+++ Country_Lab_Week_4.py
@@ -1,1 +1,10 @@
 #Ezra Felton CIS 261 Country Lab Week 4
+
+
+def display_menu():
+    print("COMMAND MENU")
+    print("view   - View country name")   
+    print("add    - Add a country")
+    print("delete - Delete a country")
+    print("exit   - Exit program ")
+    print()
